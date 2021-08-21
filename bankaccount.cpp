58e28fd450f5f94eaa7c78f7c2498a1568a91bbb
William <wms19@my.fsu.edu>
2021-08-21 11:13:42 -0400

    Replaced all cstring with string for easier copying of objects and updated menu designs

--- bankaccount.cpp
+++ bankaccount.cpp
@@ -1,65 +1,57 @@
 #include "bankaccount.h"
 using namespace std;
 
-Account::Account()
-{
-    strcpy(FirstName, "  ");
-    strcpy(LastName, "  ");
-    AccountNumber = 0;
+Account::Account(){
     BankAmount = 0.0;
 }
-Account::~Account()
-{
-    strcpy(FirstName, "  ");
-    strcpy(LastName, "  ");
-    AccountNumber = 0;
-    BankAmount = 0.0;
-}
+
+//Deconstructor empty for now could possibly be used down the road
+Account::~Account(){}
+
+//Copy Constructor 
 Account::Account(const Account& A)
 {
-   AccountNumber = A.AccountNumber;
-   for(int i = 0; i < strlen(FirstName); i++){
-        FirstName[i] = A.FirstName[i]; 
-   } 
-   for(int i = 0; i < strlen(LastName); i++){
-        LastName[i] = A.LastName[i]; 
-   } 
-   BankAmount = A.BankAmount;
-   type = A.type;
+    AccountNumber = A.AccountNumber;
+    FirstName = A.FirstName; 
+    LastName = A.LastName; 
+    BankAmount = A.BankAmount;
+    type = A.type;
 }
-void Account::Setup(){
-    char answer[20];
 
-    AccountNumber = rand() % 1000;
-    cout << "What type of account do you want: ";
-    cin.getline(answer,20);
-    for(int i = 0; i < strlen(answer);i++)
+//Initalization of the account 
+void Account::Setup(){
+    char answer;
+    srand(time(NULL));
+
+    //Random Account number for each account
+    AccountNumber = rand() % 1000 + 1;
+    
+    cout << "What type of account do you want [C for checking,S for saving]: ";
+    cin >> answer;
+    answer = toupper(answer);
+    if( answer = 'C')
     {
-        if(answer[0] == 'S' && answer[0] == 's')
-        {
-            type = Checking;
-        }
-        else if(answer[0] == 'C' && answer[0] == 'c')
-        {
-            type = Saving;
-        }
-    }
+        type = CHECKINGS;
+    }else if(answer = 'S')
+    {
+        type = SAVINGS;
+    } 
     
     cout << "Enter first name: ";
-    cin.ignore();
-    cin.getline(FirstName,30);
-   
+    cin >> FirstName;
+
     cout << "Enter last name: ";
-    cin.ignore();
-    cin.getline(LastName,30);
+    cin >> LastName; 
    
 }
+
 double Account::Deposit(double amount){
     BankAmount = BankAmount + amount;
     cout << "Amount: " << amount << " has been successfully added to your account\n";
     cout << "Thank you" << endl;
     return BankAmount; 
 }
+
 double Account::Withdraw(double amount){
     if(amount > BankAmount)
     {
@@ -70,16 +62,32 @@ double Account::Withdraw(double amount){
     cout << "Thank you" << " the amount of " << amount << " has been successfully withdrawn from your account\n";
     return BankAmount;
 }
-void Account::GetName(){
-    cout << "FirstName: "<< FirstName << endl;
-    cout << "LastName: " << LastName << endl;
+
+string Account::GetFirstName(){
+       return FirstName;
 }
+
+string Account::GetLastName(){
+    return LastName;
+}
+
 int Account::GetAccountNumber(){
    return AccountNumber;
 }
+
 double Account::GetBankAmount(){
    return BankAmount; 
 }
-AccountType Account::GetType(){
-    return type;
+
+string Account::GetType(){
+    string answer;
+    if(type == CHECKINGS)
+    {
+        answer = "Checkings";
+    } 
+    else if(type == SAVINGS)
+    {
+        answer = "Savings";
+    }
+    return answer;
 }
